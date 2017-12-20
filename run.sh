0133898f92404a46aa09e9642df8143cb5dcf69a
e11it <ilma@e11it.ru>
2017-12-20 11:39:30 +0300

    Fix

--- run.sh
+++ run.sh
@@ -41,19 +41,26 @@ graphouse.clickhouse.metric-tree-table=${GH__CLICKHOUSE__METRIC_TREE_TABLE:=metr
 graphouse.clickhouse.socket-timeout-seconds=${GH__CLICKHOUSE__SOCKET_TIMEOUT_SECONDS:=600}
 graphouse.clickhouse.query-timeout-seconds=${GH__CLICKHOUSE__QUERY_TIMEOUT_SECONDS:=120}
 graphouse.clickhouse.retention-config=${GH__CLICKHOUSE__RETENTION_CONFIG:=}
+
 #metric server and cacher
 graphouse.cacher.bind-address=${GH__CACHER__BIND_ADDRESS:=::}
 graphouse.cacher.port=${GH__CACHER__PORT:=2003}
 graphouse.cacher.threads=${GH__CACHER__THREADS:=100}
 graphouse.cacher.socket-timeout-millis=${GH__CACHER__SOCKET_TIMEOUT_MILLIS:=42000}
-graphouse.cacher.cache-size=${GH__CACHER__CACHE_SIZE:=2000000}
-graphouse.cacher.batch-size=${GH__CACHER__BATCH_SIZE:=1000000}
-graphouse.cacher.writers-count=${GH__CACHER__WRITERS_COUNT:=2}
-graphouse.cacher.flush-interval-seconds=${GH__CACHER__FLUSH_INTERVAL_SECONDS:=5}
+
+graphouse.cacher.queue-size=${GH__CACHER__QUEUE_SIZE:=10000000}
+graphouse.cacher.read-batch-size=${GH__CACHER__READ_BATCH_SIZE:=1000}
+graphouse.cacher.min-batch-size=${GH__CACHER__MIN_BATCH_SIZE:=10000}
+graphouse.cacher.max-batch-size=${GH__CACHER__MAX_BATCH_SIZE:=1000000}
+graphouse.cacher.min-batch-time-seconds=${GH__CACHER__MIN_BATCH_TIME_SECONDS:=1}
+graphouse.cacher.max-batch-time-seconds=${GH__CACHER__MAX_BATCH_TIME_SECONDS:=5}
+graphouse.cacher.max-output-threads=${GH__CACHER__MAX_OUTPUT_THREADS:=5}
+ 
 #Http server (metric search, ping, metricData)
 graphouse.http.bind-address=${GH__HTTP__BIND_ADDRESS:=::}
 graphouse.http.port=${GH__HTTP__PORT:=2005}
 graphouse.http.threads=${GH__HTTP__THREADS:=25}
+
 #Mretric search and tree
 graphouse.search.refresh-seconds=${GH__SEARCH__REFRESH_SECONDS:=60}
 graphouse.tree.in-memory-levels=${GH__TREE__IN_MOMORY_LEVELS:=3}
@@ -62,10 +69,12 @@ graphouse.tree.dir-content.cache-concurrency-level=${GH__TREE__DIR_CONTENT__CACH
 graphouse.tree.dir-content.batcher.max-parallel-requests=${GH__TREE__DIR_CONTENT__BATCHER__MAX_PARALLEL_REQUEST:=3}
 graphouse.tree.dir-content.batcher.max-batch-size=${GH__TREE__DIR_CONTENT__BATCHER__MAX_BATCH_SIZE:=2000}
 graphouse.tree.dir-content.batcher.aggregation-time-millis=${GH__TREE__DIR_CONTENT__BATCHER__AGGREGATION_TIME_MILLIS:=50}
+
 #Host metrics redirect
 graphouse.host-metric-redirect.enabled=${GH__HOST_METRICS_REDIRECT__ENABLE:=false}
 graphouse.host-metric-redirect.dir=${GH__HOST_METRICS_REDIRECT__DIR:=}
 graphouse.host-metric-redirect.postfixes=${GH__HOST_METRICS_REDIRECT__POSTFIXES:=}
+
 #Autohide
 graphouse.autohide.enabled=${GH__AUTOHIDE__ENABLED:=false}
 graphouse.autohide.run-delay-minutes=${GH__AUTOHIDE__RUN_DELAY_MINUTES:=30}
@@ -75,6 +84,7 @@ graphouse.autohide.step=${GH__AUTOHIDE__STEP:=10000}
 graphouse.autohide.retry.count=${GH__AUTOHIDE__RETRY__COUNT:=10}
 graphouse.autohide.retry.wait_seconds=${GH__AUTOHIDE__RETRY__WAIT_SECONDS:=10}
 graphouse.autohide.clickhouse.query-timeout-seconds=${GH__AUTOHIDE__CLICKHOUSE__QUERY_TIMEOUT_SECONDS:=600}
+
 #Metric validation
 graphouse.metric-validation.min-length=${GH__METRIC_VALIDATION__MIN_LENGTH:=10}
 graphouse.metric-validation.max-length=${GH__METRIC_VALIDATION__MAX_LENGTH:=200}
